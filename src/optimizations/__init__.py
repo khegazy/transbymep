e5bb701405ee6fbd1bed4decebd44546a04d756a
khegazy <KareemHegz@gmail.com>
2024-04-08 22:15:34 -0700

    Removed or moved files.

--- src/optimizations/__init__.py
+++
@@ -1,9 +0,0 @@
-from .gradient_descent import gradientDescent
-
-optimizer_dict = {
-    "gradient_descent" : gradientDescent
-}
-
-def get_optimizer(name):
-    assert(name.lower() in optimizer_dict)
-    return optimizer_dict[name.lower()]
--- src/tools/initialize.py
+++
@@ -1,8 +0,0 @@
-import jax
-import jax.numpy as jnp
-import numpy as np
-
-def compute_initial_points(start, end, number_of_points):
-    ts = np.linspace(0.0, 1.0, number_of_points+1)[1:]
-    points = [ start * ( 1 - t ) + end * t for t in ts ]
-    return jnp.stack(points)

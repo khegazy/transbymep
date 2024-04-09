5354d4a5897120e5d2c5b6351818f65b3515db18
khegazy <KareemHegz@gmail.com>
2024-04-08 22:32:52 -0700

    Added options for optimization and cleaned up.

--- src/tools/arg_parser.py
+++ src/tools/arg_parser.py
@@ -7,9 +7,19 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
 
     # Name, seed, directory
     parser.add_argument("--name", help="experiment name", required=True)
-    parser.add_argument("--potential_tag", help="potential name", type=str, default=None, required=False)
-    parser.add_argument("--path_tag", help="path tag", type=str, default="", required=False)
-    parser.add_argument("--tag", help="run tag", type=str, default="", required=False)
+    parser.add_argument(
+        "--potential_tag",
+        help="potential name",
+        type=str,
+        default=None,
+    )
+    parser.add_argument(
+        "--path_tag", 
+        help="path tag", 
+        type=str, 
+        default="", 
+    )
+    parser.add_argument("--tag", help="run tag", type=str, default="")
     parser.add_argument("--seed", help="random seed", type=int, default=123)
     parser.add_argument(
         "--output_dir",
@@ -25,6 +35,12 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
         type=str,
         default="mlp",
         required=False
+    )
+    parser.add_argument(
+        '--randomly_initialize_path',
+        default=False,
+        action='store_true',
+        help="Will randomly initialize path"
     )
 
     # Chemical potential
@@ -45,6 +61,14 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
     )
     parser.add_argument(
         '--minimize_end_points', default=False, action='store_true'
+    )
+    parser.add_argument(
+        '--num_optimizer_iterations', default=10000, type=int
+    )
+
+    # Plotting
+    parser.add_argument(
+        '--make_animation', default=False, action='store_true'
     )
 
     # Debuging and testing

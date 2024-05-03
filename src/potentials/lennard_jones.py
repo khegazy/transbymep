53d9d784291a7ced8df29d319c34597a7aca8ee5
Eric Yuan <ericyuan@berkeley.edu>
2024-05-02 17:11:15 -0700

    Revise Lennard-Jones parameters

--- src/potentials/lennard_jones.py
+++ src/potentials/lennard_jones.py
@@ -15,13 +15,11 @@ class LennardJones(BasePotential):
         """
         pos = points.unflatten(-1, (-1, 3))
         # dist = torch.linalg.norm(pos[..., None, :, :] - pos[..., :, None, :], dim=-1)
-        # inv_dist = 1 / (dist + 1e-6)
-        # inv_dist = torch.where(dist > 0, inv_dist, torch.zeros_like(dist))
-        # energy = 4 * torch.sum(inv_dist ** 12 - inv_dist ** 6, dim=(-1, -2)) / 2
+        # energy = 4 * self.epsilon * torch.sum(((self.sigma / dist) ** 12 - (self.sigma / dist) ** 6).triu(diagonal=1), dim=(-1, -2))
         energy = 0.0
         for i in range(pos.shape[-2]):
             for j in range(i + 1, pos.shape[-2]):
                 dist = torch.linalg.norm(pos[..., i, :] - pos[..., j, :], dim=-1)
-                inv_dist = 1 / dist
-                energy += 4 * (inv_dist ** 12 - inv_dist ** 6)
+                inv_dist = self.sigma / dist
+                energy += 4 * self.epsilon * (inv_dist ** 12 - inv_dist ** 6)
         return energy

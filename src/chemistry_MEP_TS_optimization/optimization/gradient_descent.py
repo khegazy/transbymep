f25331d001d47a2ac5cc8803976c7e514b9a699c
Anup Kumar <anukumar94663@gmail.com>
2024-05-10 00:59:54 -0700

    Added the right hand sides of these two lines in the gradient descent class. self.path = path self.integrator = integrator
    Also, added type hints and NumPy style strings.

--- src/chemistry_MEP_TS_optimization/optimization/gradient_descent.py
+++ src/chemistry_MEP_TS_optimization/optimization/gradient_descent.py
@@ -13,27 +13,44 @@ def update(params, grad_fxn, metrics, learning_rate):
         lambda param, g: param - g*learning_rate, params, grads
     )
 
+
 class gradientDescent:
+    """
+    Gradient descent optimizer.
+
+    Attributes:
+        path (object): The path to optimize.
+        integrator (object): The integrator to compute gradients.
+        loss_fxn (callable): The loss function to minimize.
+        config (dict): Configuration parameters.
+        max_n_steps (int): Maximum number of optimization steps.
+    """
     def __init__(
             self,
             path,
             integrator,
             loss_fxn,
             config,
-            max_n_steps=1e9,
-    ):
-        self.path
-        self.integrator
+            max_n_steps: int = int(1e9),
+    ) -> None:
+        self.path = path
+        self.integrator = integrator
         self.loss_fxn = loss_fxn
         self.config = config
         self.max_n_steps = max_n_steps
 
     def find_critical_path(
             self,
-            n_steps=10,
-            log_frequency=1000
-    ):
+            n_steps: int = 10,
+            log_frequency: int = 1000
+    ) -> None:
+        """
+        Find the critical path using gradient descent optimization.
 
+        Args:
+            n_steps (int, optional): Number of optimization steps. Defaults to 10.
+            log_frequency (int, optional): Frequency of logging. Defaults to 1000.
+        """
         print("computing critical_path...")
         n_steps = n_steps if n_steps is not None else self.max_n_steps
 
@@ -57,13 +74,18 @@ class gradientDescent:
                 )
             """
 
-    
     @partial(jax.jit, static_argnums=[0])
-    def update_path(self):
+    def update_path(self) -> float:
+        """
+        Update the path weights using gradient descent.
+
+        Returns:
+            float: Loss value after the update.
+        """
         loss = self.loss_fxn(self.path, self.potential)
         grads = jax.grad(loss)(self.path.weights)
-        self.path.weights = [(w - self.step_size*dw, b - self.step_size*db)\
-            for (w,b), (dw, db) in zip( self.path.weights, grads)]
+        self.path.weights = [(w - self.step_size*dw, b - self.step_size*db)
+                             for (w,b), (dw, db) in zip( self.path.weights, grads)]
         """
         new_points = points -  self.path_step_factor*jax.grad(
             self.action, argnums=1)(

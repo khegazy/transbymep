Fix potential calculation

transbymep/paths/base_path.py
@@ -5,6 +5,7 @@ from transbymep.potentials.base_class import PotentialBase
 from typing import Callable, Any
 from ase import Atoms
 from ase.io import read, write
+import numpy as np
 
 
 @dataclass
@@ -133,7 +134,7 @@ class BasePath(torch.nn.Module):
             self.initial_point = initial_point
             self.final_point = final_point
             self.vec = self.final_point - self.initial_point
-            self.wrap_fn = None
+            self.transform = None
         elif isinstance(initial_point, Atoms) or isinstance(initial_point, str):
             if isinstance(initial_point, str):
                 initial_point = read(initial_point)
@@ -167,7 +168,7 @@ class BasePath(torch.nn.Module):
             self.vec = torch.tensor(
                 [pair.get_distance(i, i + self.n_atoms, mic=True, vector=True) for i in range(self.n_atoms)], dtype=torch.float64, device=device
             ).flatten()
-            self.wrap_fn = self.wrap_points if self.pbc.any() else None
+            self.transform = self.wrap_points if self.pbc.any() else None
         else:
             raise ValueError("Invalid type for initial_point and final_point.")
 
@@ -181,7 +182,7 @@ class BasePath(torch.nn.Module):
 
         fractional[..., :, self.pbc] %= 1.0
 
-        return torch.matmul(fractional, self.cell)
+        return torch.matmul(fractional, self.cell).view(*points.shape)
 
 
     def geometric_path(
@@ -266,8 +267,8 @@ class BasePath(torch.nn.Module):
             t = torch.unsqueeze(t, -1)
         t = t.to(torch.float64).to(self.device)
         geo_path = self.geometric_path(t)
-        if self.wrap_fn is not None:
-            geo_path = self.wrap_fn(geo_path)
+        if self.transform is not None:
+            geo_path = self.transform(geo_path)
         # traj = [Atoms(
         #         numbers=self.numbers.detach().cpu().numpy(), 
         #         positions=pos.reshape(self.n_atoms, 3).detach().cpu().numpy(),
@@ -276,6 +277,7 @@ class BasePath(torch.nn.Module):
         #     ) for pos in geo_path]
         # write("test.xyz", traj)
         # raise ValueError("STOP")
+        print("GEOPATH", geo_path.shape)
         pes_path = self.potential(geo_path)
 
         velocity, force = None, None

transbymep/potentials/newtonnet.py
@@ -27,8 +27,6 @@ class NewtonNetPotential(PotentialBase):
         """
         super().__init__(**kwargs)
         self.model = self.load_model(os.path.join(config_dir, model_path))
-        # self.numbers = np.array(numbers)
-        # self.n_atoms = len(numbers)
         self.n_eval = 0
 
     
@@ -51,7 +49,8 @@ class NewtonNetPotential(PotentialBase):
         n_data = pos.numel() // (self.n_atoms * 3)
         data  = {
             'R': pos.view(n_data, self.n_atoms, 3),
-            'Z': np.stack([self.numbers for _ in range(n_data)]),
+            'Z': self.numbers.repeat(n_data, 1).cpu().numpy(),
+            # 'Z': np.stack([self.numbers for _ in range(n_data)]),
             # 'E': torch.zeros((1, 1)),
             # 'F': torch.zeros((1, len(self.numbers), 3)),
         }
@@ -61,4 +60,6 @@ class NewtonNetPotential(PotentialBase):
         data['NM'] = torch.tensor(NM, device=self.device)
         data['AM'] = torch.tensor(AM, device=self.device)
         # data = batch_dataset_converter(data, device=self.device)
+        # print(data)
+        # raise ValueError
         return data
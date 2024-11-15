5f73a727c39ffcdcb27269d535ab59919d052d47
ericyuan00000 <ericyuan@berkeley.edu>
2024-11-15 14:29:20 -0800

    [wip] Modify path NN architecture

--- transbymep/paths/mlp.py
+++ transbymep/paths/mlp.py
@@ -34,18 +34,20 @@ class MLPpath(BasePath):
     ):
         super().__init__(**kwargs)
 
-        # self.activation = activation_dict[activation]
+        self.activation = activation_dict[activation]
         # input_sizes = [1] + [n_embed]*(depth - 1)
-        # input_sizes = [1] + [self.final_point.shape[-1] * n_embed]*(depth - 1)
-        # output_sizes = input_sizes[1:] + [self.final_point.shape[-1]]
-        # output_sizes = [size * 2 for size in input_sizes[1:]] + [self.final_point.shape[-1]]
-        # self.layers = [
-        #     nn.Linear(
-        #         input_sizes[i//2], output_sizes[i//2], dtype=torch.float64, bias=True
-        #     ) if i%2 == 0 else self.activation\
-        #     for i in range(depth*2 - 1)
-        # ]
-        self.layers = [nn.Linear(1, self.final_point.shape[-1], dtype=torch.float64, bias=True)] + [ResNetLayer(self.final_point.shape[-1], n_embed, activation) for i in range(depth)]
+        input_sizes = [1] + [self.final_point.shape[-1] * n_embed]*(depth - 1)
+        output_sizes = input_sizes[1:] + [self.final_point.shape[-1]]
+        self.layers = [
+            nn.Linear(
+                input_sizes[i//2], output_sizes[i//2], dtype=torch.float64, bias=True
+            ) if i%2 == 0 else self.activation\
+            for i in range(depth*2 - 1)
+        ]
+        # self.layers = [nn.Linear(1, self.final_point.shape[-1], dtype=torch.float64, bias=True)]
+        # for i in range(depth):
+        #     # self.layers.append(ResNetLayer(self.final_point.shape[-1]))
+        #     self.layers.append(ResNetLayer(self.final_point.shape[-1], n_embed, activation))
         # self.input_layers = nn.Sequential(
         #     nn.Linear(1, n_embed, bias=True),
         #     self.activation,
@@ -99,7 +101,7 @@ class MLPpath(BasePath):
         # n_data = len(time)
         # mlp_out = self.mlp(time) - (1 - time) * self.mlp(self.t_init) - time * self.mlp(self.t_final)
         # mlp_out = self.mlp(time) * torch.sin(time * torch.pi)
-        mlp_out = self.mlp(time) * (1 - time) * time
+        mlp_out = self.mlp(time) * (1 - time) * time #* 4
         # mlp_out = self.mlp[0](time)
         # for layer in self.mlp[1:-1]:
         #     mlp_out = mlp_out + layer(mlp_out)
@@ -109,7 +111,9 @@ class MLPpath(BasePath):
         # mlp_out = mlp_out.view(n_data, self.n_atoms, 3)
         # mlp_out = mlp_out * (self.tags != 0)[None, :, None]
         # mlp_out = mlp_out.view(n_data, self.n_atoms * 3)
-        out = self.base.get_geometry(time) + mlp_out
+        base_out = self.base.get_geometry(time) #* (1 - (1 - time) * time * 4)
+        out = base_out + mlp_out
+        # out = mlp_out
         return out
 
 class ResNetLayer(nn.Module):
@@ -126,6 +130,17 @@ class ResNetLayer(nn.Module):
             self.activation,
             nn.Linear(output_size * n_embed, output_size, dtype=torch.float64, bias=True),
         )
+        # self.layer = SwiGLU(output_size, output_size)
 
     def forward(self, x):
         return x + self.layer(x)
+    
+class SwiGLU(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.fc1 = nn.Linear(in_features, out_features)
+        self.fc2 = nn.Linear(in_features, out_features)
+        self.activation = nn.SiLU()
+
+    def forward(self, x):
+        return self.fc1(x) * self.activation(self.fc2(x))

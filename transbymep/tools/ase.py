56454170007715c34efa6402e346533bc77646eb
ericyuan00000 <ericyuan@berkeley.edu>
2025-03-06 10:18:20 -0800

    Update atoms record

--- transbymep/tools/ase.py
+++ transbymep/tools/ase.py
@@ -1,3 +1,4 @@
+import ase
 import torch
 
 
@@ -56,5 +57,62 @@ def pair_displacement(initial_atoms, final_atoms):
         Pair displacement.
     """
     pair = initial_atoms + final_atoms
-    vec = [pair.get_distance(i, i + len(initial_atoms), mic=True, vector=True) for i in range(len(initial_atoms))]
-    return torch.tensor(vec, dtype=torch.float64)
+    vec = pair.get_distances(
+        [i for i in range(len(initial_atoms))],
+        [i + len(initial_atoms) for i in range(len(initial_atoms))],
+        mic=True,
+        vector=True,
+    )
+    return vec
+
+def output_to_atoms(output, ref_images):
+    """
+    Convert output to ase.Atoms.
+    
+    Parameters:
+    -----------
+    output : paths.PathOutput
+        Path output.
+    ref_images : tools.Images
+        Reference images.
+
+    Returns:
+    --------
+    list[ase.Atoms]
+        List of Atoms objects.
+    """
+    images = []
+    for positions, energy, velocities, forces in zip(output.path_geometry, output.path_energy, output.path_velocity, output.path_force):
+        atoms = ase.Atoms(
+            numbers=ref_images.numbers.detach().cpu().numpy(),
+            positions=positions.detach().cpu().numpy().reshape(-1, 3),
+            velocities=velocities.detach().cpu().numpy().reshape(-1, 3),
+            pbc=ref_images.pbc.detach().cpu().numpy(),
+            cell=ref_images.cell.detach().cpu().numpy(),
+            tags=ref_images.tags.detach().cpu().numpy(),
+        )
+        calc = SinglePointCalculator(
+            atoms,
+            energy=energy.detach().cpu().numpy(),
+            # forces=forces.detach().cpu().numpy().reshape(-1, 3),
+        )
+        atoms.calc = calc
+        images.append(atoms)
+    return images
+
+def wrap_points(
+        points: torch.Tensor,
+        cell: torch.Tensor,
+        center: float = 0
+    ) -> torch.Tensor:
+    """
+    PyTorch implementation of ase.geometry.wrap_positions function.
+    Assume periodic boundary conditions for all dimensions.
+    """
+
+    fractional = torch.linalg.solve(cell.T, points.view(*points.shape[:-1], -1, 3).transpose(-1, -2)).transpose(-1, -2)
+
+    # fractional[..., :, self.pbc] %= 1.0
+    fractional = (fractional + center) % 1.0 - center    # TODO: Modify this to handle partially true PBCs
+
+    return torch.matmul(fractional, cell).view(*points.shape)

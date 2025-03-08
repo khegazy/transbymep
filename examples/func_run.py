3140fc9a6e21cc84cf0195c41cc5870ae2d25c2e
ericyuan00000 <ericyuan@berkeley.edu>
2025-03-07 16:20:12 -0800

    Update example scripts

--- examples/func_run.py
+++ examples/func_run.py
@@ -1,49 +1,47 @@
 from ase.io import read, write
 from popcornn import optimize_MEP
 
-
-# Create configurations, or read them from a file
-config = {
-    "potential_params": {
-        "potential": "repel",
-        "alpha": 1.7,
-        "beta": 0.01,
-    },
-    "path_params": {
-        "name": "mlp",
-        "n_embed": 16,
-        "depth": 3,
-        "activation": "gelu",
-    },
-    "integrator_params": {
-        "method": "dopri5",
-        "rtol": 1.0e-5,
-        "atol": 1.0e-5,
-        "computation": "parallel",
-        "sample_type": "uniform",
-        "path_loss_name": "integral",
-        "path_ode_names": "E_geo",
-    },
-    "optimizer_params": {
-        "optimizer": {
-            "name": "adam",
-            "lr": 1.0e-3,
+if __name__ == "__main__":
+    # Create configurations, or read them from a file
+    config = {
+        "potential_params": {
+            "potential": "repel",
         },
-        "lr_scheduler": {
-            "name": "cosine",
-            "T_max": 1000,
-        }
-    },
-    "num_optimizer_iterations": 1000,
-    "num_record_points": 20,
-}
+        "path_params": {
+            "name": "mlp",
+            "n_embed": 16,
+            "depth": 3,
+            "activation": "gelu",
+        },
+        "integrator_params": {
+            "method": "dopri5",
+            "rtol": 1.0e-5,
+            "atol": 1.0e-5,
+            "computation": "parallel",
+            "sample_type": "uniform",
+            "path_loss_name": "integral",
+            "path_ode_names": "E_geo",
+        },
+        "optimizer_params": {
+            "optimizer": {
+                "name": "adam",
+                "lr": 1.0e-3,
+            },
+            "lr_scheduler": {
+                "name": "cosine",
+                "T_max": 1000,
+            }
+        },
+        "num_optimizer_iterations": 1000,
+        "num_record_points": 20,
+    }
 
-# Read the initial images
-initial_images = read('configs/6445.xyz', index=':')
+    # Read the initial images
+    initial_images = read('configs/6445.xyz', index=':')
 
-# Run the optimization
-final_images, ts_image = optimize_MEP(initial_images, **config)
+    # Run the optimization
+    final_images, ts_image = optimize_MEP(initial_images, **config)
 
-# Write the final images
-write('configs/6445_popcornn.xyz', final_images)
+    # Write the final images
+    write('configs/6445_popcornn.xyz', final_images)
 
--- examples/run.py
+++ examples/run.py
@@ -1,3 +1,4 @@
+from ase.io import read, write
 from popcornn import tools, optimize_MEP
 
 
@@ -9,5 +10,9 @@ if __name__ == "__main__":
     # Import configuration files
     args = tools.build_default_arg_parser().parse_args()
     config = tools.import_run_config(args.config)
-        
-    output = optimize_MEP(**config)
+    
+    # Run the optimization
+    final_images, ts_image = optimize_MEP(**config)
+    
+    # Write the final images
+    write('configs/6445_popcornn.xyz', final_images)
